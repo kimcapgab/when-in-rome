a6b77a1cd089bda873f80c621e8411a6f0e10d95
Kim Capilla <kcapilla@Kims-MBP.attlocal.net>
2022-03-03 23:40:50 -0500

    functioning front end for number to roman

--- when-in-rome/pages/api/convert.js
+++
@@ -1,5 +0,0 @@
-import roman from "/int_to_roman.json"
-
-export default function handler(req, res) {
-  res.status(200).json(roman)
-}
---
+++ when-in-rome/pages/api/numbers/num.js
@@ -0,0 +1,15 @@
+import roman from "/int_to_roman.json";
+
+export default function convert(req, res) {
+  let num = req;
+  let result = [];
+
+  roman.forEach(function (convert) {
+    while (num >= convert[1]) {
+      result.push(convert[0]);
+      num -= convert[1];
+    }
+    return result.join("");
+  });
+  return result.join("");
+}
